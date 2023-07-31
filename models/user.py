6790bf09fc4c01c08faae1fb3ce95b4323ec9c7f
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-30 22:32:36 -0300

    fix model user: change nullable to first and last names

--- models/user.py
+++ models/user.py
@@ -13,8 +13,8 @@ class User(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
         places = relationship(
             'Place', backref='user', cascade='all, delete, delete-orphan')
