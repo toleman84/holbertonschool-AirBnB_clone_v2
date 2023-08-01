08fe740a3609da2c037ad2f21b80adf1723bb765
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-01 03:54:08 -0300

    add task 5: Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+    all_classes = {'BaseModel': BaseModel, 'User': User,
+                   'State': State, 'City': City, 'Amenity': Amenity,
+                   'Place': Place, 'Review': Review}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+
+        dictionary = {}
+        if cls.__name__ in self.all_classes:
+            for key in self.__objects.keys():
+                split = key.split('.')
+                if split[0] == cls.__name__:
+                    dictionary[key] = self.__objects[key]
+        return dictionary
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +64,14 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects"""
+        if obj is None:
+            return
+        else:
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
+            del self.__objects[key]
