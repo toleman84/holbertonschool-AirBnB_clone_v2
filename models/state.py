93525241835367efd902f29ce841194530dd7cf3
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-02 09:45:59 -0300

    fix

--- models/state.py
+++ models/state.py
@@ -7,5 +7,5 @@ from sqlalchemy import Column, String
 
 class State(BaseModel, Base):
     """ State class """
-    # __tablename__ = 'states'
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
