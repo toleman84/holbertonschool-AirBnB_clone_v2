48262953b678c440a4e5325ff3cdd64353cb7959
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-02 09:33:08 -0300

    fix: extract code for compile

--- models/city.py
+++ models/city.py
@@ -8,5 +8,5 @@ from sqlalchemy import Column, String, ForeignKey
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state_id = Column(String(60), nullable=False) #ForeignKey('states.id'),
     name = Column(String(128), nullable=False)
