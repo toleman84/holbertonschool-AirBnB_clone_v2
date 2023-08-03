1da9f3b63ece202c340059253049b513501a902e
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-03 20:23:05 -0300

    fix place: add Base class inherit

--- models/place.py
+++ models/place.py
@@ -7,7 +7,7 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey
 #from os import getenv
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
