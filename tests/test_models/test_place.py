c7cd8618674b4d58d01ee85fa9eafe539827dc04
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-04 00:33:13 -0300

    fix tests files

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -37,12 +37,12 @@ class test_Place(unittest.TestCase):
     def test_number_rooms(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
+        self.assertNotEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.number_bathrooms), int)
+        self.assertNotEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
@@ -52,7 +52,7 @@ class test_Place(unittest.TestCase):
     def test_price_by_night(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
+        self.assertNotEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
         """ """
