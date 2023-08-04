edf42c47288d4c9b63bfe2c4dde17c69641592fd
fabianatellechea <fabianatellechea@gmail.com>
2023-08-03 18:17:49 -0700

    table added

--- models/place.py
+++ models/place.py
@@ -2,10 +2,12 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 import models
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 #from sqlalchemy.orm import relationship
 #from os import getenv
 
+metadata = Base.metadata
+place_amenity = Table('place_amenity', metadata, Column('Place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False), Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 class Place(BaseModel):
     """ A place to stay """
