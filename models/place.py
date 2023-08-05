9ed0bb16b5ed8925eb27fb63858d75eff0e89d28
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-05 20:07:31 -0300

    fix pep8

--- models/place.py
+++ models/place.py
@@ -35,7 +35,8 @@ class Place(BaseModel, Base):
         reviews = relationship('Review', cascade='all, delete, delete-orphan',
                                backref='place')
         amenities = relationship('Amenity',
-                                 secondary=place_amenity, viewonly=False, back_populates='place_amenities')
+                                 secondary=place_amenity, viewonly=False,
+                                 back_populates='place_amenities')
     else:
         @property
         def reviews(self):
