10c77817f434ece03bb6ab2652a0afa5a994eeda
fabianatellechea <fabianatellechea@gmail.com>
2023-08-05 13:12:31 -0700

    getter added

--- models/state.py
+++ models/state.py
@@ -2,9 +2,29 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+
+        cities = relationship('City', cascade='all, delete, delete-orphan',
+                              backref='state')
+
+    else:
+        @property
+        def cities(self):
+            """ returns the list of City instances with
+            state_id equals to the current State.id """
+            cities_list = []
+
+            for key, value in self.all().items():
+                if value.__class__.__name__ == 'City':
+                    if value.state_id == self.id:
+                        cities_list.append(value)
+            return cities_list
