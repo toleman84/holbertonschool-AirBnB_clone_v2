0e04c8afaf77ff3a532725ea0c282dac9e9b22bb
fabianatellechea <fabianatellechea@gmail.com>
2023-08-05 12:02:52 -0700

    syntax fix

--- models/place.py
+++ models/place.py
@@ -34,7 +34,7 @@ class Place(BaseModel, Base):
         reviews = relationship('Review', cascade='all, delete, delete-orphan',
                                backref='place')
         amenities = relationship('Amenity',
-                                 secondary='place_amenity', viewonly=False)
+                                 secondary=place_amenity, viewonly=False, back_populates='place_amenities')
     else:
         @property
         def reviews(self):
