4d96e1b8b636487a40b4f6e2b0bbc2167e3be1a8
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-05 22:28:36 -0300

    fix model: place

--- models/place.py
+++ models/place.py
@@ -38,6 +38,18 @@ class Place(BaseModel, Base):
                                  secondary=place_amenity, viewonly=False,
                                  back_populates='place_amenities')
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
             """returns the list of Review"""
