42c49bb23c1c5d322eb3f87931a2c6e63695793a
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-05 23:40:59 -0300

    fix test: test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,48 +6,8 @@ from console import HBNBCommand
 
 class TestHBNBCommand(unittest.TestCase):
 
-    def test_precmd(self):
-        """_summary_
-        """
-        pass
-
-    def test_do_create(self):
-        """_summary_
-        """
-        pass
-
-    def test_do_show(self):
-        """_summary_
-        """
-        pass
-
-    def test_do_destroy(self):
-        """_summary_
-        """
-        pass
-
-    def test_do_all(self):
-        """_summary_
-        """
-        pass
-
-    def test_do_update(self):
-        """_summary_
-        """
-        pass
-
     def test_emptyline(self):
         """_summary_
         """
         test_obj = HBNBCommand()
         self.assertEqual(test_obj.emptyline(), None)
-
-    def test_help(self):
-        """_summary_
-        """
-        pass
-
-    def test_quit(self):
-        """_summary_
-        """
-        pass
