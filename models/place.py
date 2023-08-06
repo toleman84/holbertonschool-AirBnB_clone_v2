5a93079c190e069ca9f53ffa5c1d93ad20be0d55
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-05 21:28:12 -0300

    fix model: place

--- models/place.py
+++ models/place.py
@@ -32,12 +32,24 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship('Review', cascade='all, delete, delete-orphan',
-                               backref='place')
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete, delete-orphan')
         amenities = relationship('Amenity',
                                  secondary=place_amenity, viewonly=False,
                                  back_populates='place_amenities')
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
             """returns the list of Review"""
