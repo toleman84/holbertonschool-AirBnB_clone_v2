2c92ff23ad70ed66c63b0d7e34414162e67aa88f
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-05 21:17:13 -0300

    fix model: place

--- models/place.py
+++ models/place.py
@@ -62,3 +62,15 @@ class Place(BaseModel, Base):
                 self.amenity_ids.append(obj.id)
             else:
                 pass
+
+        city_id = ''
+        user_id = ''
+        name = ''
+        description = ''
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
