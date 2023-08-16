a50f8a2111c3c5471a92b04412b4a865b969f00d
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-16 00:16:42 -0300

    add exercises 0-5

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""doc"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Hello Flask!"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """replace underscore _ symbols with a space"""
+    return "C " + text.replace("_", " ")
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Python is cool!"""
+    return "Python " + text.replace("_", " ")
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    """Is it a number?"""
+    if isinstance(n, int):
+        return ("{} is a number".format(n))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
