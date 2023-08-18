e00e78f4713545b999bf9ea5befc518aac696912
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-18 00:10:39 -0300

    add exercise 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
 """doc"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session():
+    """used to close the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """doc"""
+    states = storage.all(State).order_by(State.name)
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """doc"""
+    state = storage.get(State, id)
+    if state is not None:
+        cities = state.cities
+        return render_template("9-states.html", state=state, cities=cities)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
