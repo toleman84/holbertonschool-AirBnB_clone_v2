971f257870f9951a0c27f626e09a57bf23ab8faf
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-18 01:24:33 -0300

    add exercise 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
 """doc"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session():
+    """used to close the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """doc"""
+    states = storage.all(State).order_by(State.name)
+    amenities = storage.all(Amenity).order_by(Amenity.name)
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
