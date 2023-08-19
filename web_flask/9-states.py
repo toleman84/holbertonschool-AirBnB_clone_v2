02fc7e780785a09cd465a8a19f469e4346bb238c
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-19 15:26:10 -0300

    fix exercise 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -27,7 +27,7 @@ def states_id(id):
     state = storage.all(State)
     if state is not None:
         cities = state.cities
-        return render_template("9-states.html", state=state.values(), cities=cities)
+        return render_template("9-states.html", state=state.values(), cities=cities.values())
 
 
 if __name__ == "__main__":
